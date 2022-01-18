bec17b93878538c74b96fe726099d10d5defe8a2
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2022-01-18 19:03:48 +0600

    data laod

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Article = () => {
+    const [articles , setArticles] = useState([]);
+    const [loading , setLoading] = useState(false);
+    useEffect( () => {
+        // setTimeout( () => {
+            // setLoading(true);
+            // console.log(loading);
+            // fetch('https://jsonplaceholder.typicode.com/posts')
+            // .then( res => res.json())
+            // .then( data => {
+            //     setLoading(false);
+            //     console.log(loading);
+            //     setArticles(data);
+                
+            // })
+        // } , 3000)
+        
+        setLoading(true)
+
+        setTimeout( () => {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then( data => {
+                setArticles(data.data);
+                setLoading(false)
+            })
+        } ,3000)
+
+    } , [])
     return (
-        <div>
-            <h2>Article js</h2>
-        </div>
+        <>
+            {
+                loading ? (
+                    <div>Loading</div>
+                ) : (
+                    <div>
+                       {
+                           articles?.map( article => {
+                               return(
+                                   <div>
+                                        <h3>{article?.title}</h3>
+                                        <p>{article?.body}</p>
+                                   </div>
+                               )
+                           })
+                        
+                        }
+                    </div>
+                )
+            }
+        </>
     );
 };
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 
-const user = () => {
+const User = () => {
+
+    //get user from api
+    const [user, setUser] = useState(null);
+
+    useEffect( () => {
+        setTimeout(  () => {
+
+            fetch('https://jsonplaceholder.typicode.com/users/1')
+            .then(response => response.json())
+            .then(data => setUser(data));
+
+        } ,3000)
+    } ,[])
+
     return (
-        <div>
-            <h2>User</h2>
-        </div>
+        <>
+            {
+                user ? (
+                    <div className="profile">
+                    <h2>User: {user?.name}</h2>
+                    <p>Email : {user?.email}</p>
+                    <a href={user?.website}>Website : {user?.website}</a>
+            </div>
+                ) : (
+                    <div className="loading">Loading</div>
+                )
+            }
+        </>
     );
 };
 
-export default user;
+export default User;
